4c2efc1c881e064370b0cf2ce8940a6267206df2
Mr Smoker <97607922+SmokerMusicX@users.noreply.github.com>
2022-02-16 07:54:31 +0530

    Update broadcast.py

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -1,18 +1,26 @@
+""" broadcast & statistic collector """
+
 # Copyright (C) 2021 By adityaProject
-# Originally written by levina on github
+# Originally written by aditya on github
 # Broadcast function
 
 
 import asyncio
-
+import traceback
 from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
-
+from helpers.filters import command
 from callsmusic.callsmusic import client as aditya
 from config import SUDO_USERS
+from helpers.decorators import sudo_users_only
+from database.dbchat import get_served_chats
+from database.dbusers import get_served_users
+from database.dbpunish import get_gbans_count
+from database.dbqueue import get_active_chats
 
-@Client.on_message(filters.command(["gcast"]))
+
+@Client.on_message(filters.command(["acast"]))
 async def broadcast(_, message: Message):
     sent=0
     failed=0
@@ -33,3 +41,68 @@ async def broadcast(_, message: Message):
             except:
                 failed=failed+1
         await message.reply_text(f"`gcast succesfully` \n\n**sent to:** `{sent}` chats \n**failed in:** {failed} chats")
+
+    
+
+
+@Client.on_message(command(["bcast"]) & ~filters.edited)
+@sudo_users_only
+async def broadcast_message_pin(c: Client, message: Message):
+    aditya = await message.reply_text("Starting ...")
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await c.forward_messages(i, y, x)
+                try:
+                    await m.pin(disable_notification=True)
+                    pin += 1
+                except Exception:
+                    pass
+                await asyncio.sleep(0.3)
+                sent += 1
+                await aditya.edit(f"Sent To - {sent}")
+            except Exception:
+                pass
+        await message.reply_text(
+            f"✅ Broadcast complete in {sent} Group.\n📌 Sent with {pin} chat pins."
+        )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast_pin (`message`) or (`reply to message`)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    pin = 0
+    chats = []
+    schats = await get_served_chats()
+    aditya = await message.reply_text("Starting ...")
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await c.send_message(i, text=text)
+            try:
+                await m.pin(disable_notification=True)
+                pin += 1
+            except Exception:
+                pass
+            await asyncio.sleep(0.3)
+            sent += 1
+            await aditya.edit(f"Sent To - {sent}")
+        except Exception:
+            pass
+    await message.reply_text(
+        f"✅ Broadcast complete in {sent} Group.\n📌 Sent with {pin} chat pins."
+    )
